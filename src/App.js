af8a3889b660b91b3d7b1c0491417cdcb5010c56
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-07-04 18:59:38 +0200

    navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
 import './App.css';
+import Navbar from './components/Navbar';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Details from './components/Details';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-  hello
-    </div>
+    <>
+      <Router >
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/*" element={<Error />} />        
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/cart">Cart</Link>
+     
+    </div>
+  )
+}
+
