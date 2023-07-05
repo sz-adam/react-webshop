8e526fb6bedb8a44bd7c3a46d10b813f4249e313
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-07-05 16:59:50 +0200

    navbar +css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/Navbar.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { AiOutlineClose } from 'react-icons/ai'
 
 export default function Navbar() {
+  const [mobile, setMobile] = useState(false)
+
+  const closeMobileMenu = () => {
+    setMobile(false);
+  }
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/cart">Cart</Link>
-     
+    <div className='navbar'>
+      <div className={mobile ? 'nav-links-mobile' : 'nav-links'}>
+        <Link to="/" className="navLink"onClick={closeMobileMenu} >
+          {mobile ? 'Home' :<AiOutlineHome />}
+        </Link>
+        <Link to="/cart" className="navLink" onClick={closeMobileMenu}>
+          {mobile ? 'Cart' :  <AiOutlineShoppingCart />}
+        </Link>
+      </div>
+      <button className='mobile-menu-icon' onClick={() => setMobile(!mobile)}>
+        {mobile ? <AiOutlineClose />: <GiHamburgerMenu /> }
+      </button>
     </div>
   )
 }
