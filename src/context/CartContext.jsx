478327af410a117d5cbb18dd92aa8d22691815cc
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-07-06 20:19:27 +0200

    footer repair + cart length link

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,17 +3,19 @@ import React, { useState, createContext } from 'react';
 
 export const CartContext = createContext();
 
-export const CartProvider = ({ children}) => {
+export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
- console.log(cart)
-  const addToCart = (product) => {  
+  console.log(cart)
+
+  
+  const addToCart = (product) => {
     setCart([...cart, { ...product, quantity: 1 }]);
   };
 
 
 
   return (
-    <CartContext.Provider value={{ cart, addToCart}}>
+    <CartContext.Provider value={{ cart, addToCart }}>
       {children}
     </CartContext.Provider>
   );
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,35 +3,46 @@ import { CartContext } from '../context/CartContext';
 import '../styles/Cart.css'
 import { AiFillMinusCircle } from 'react-icons/ai'
 import { AiFillPlusCircle } from 'react-icons/ai'
-import {AiFillDelete } from 'react-icons/ai'
+import { AiFillDelete } from 'react-icons/ai'
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
   const { cart } = useContext(CartContext);
   return (
     <div className='cartContainer'>
-      {cart.map((cartItem, index) => (
-        <div key={index} className='cartItem'>
-          <div className='cartImage'>
-            <img
-              src={cartItem.image}
-              alt={cartItem.name}
-            />
-          </div>
-          <div className='cartTitle'>
-            <p>{cartItem.title.substring(0, 20)}...</p>
-            <p>${cartItem.price}</p>
-            <div className='cartButtons'>
-              <button className='cartButton'><AiFillMinusCircle /></button>
-              <span className='cartSpan'>1</span>
-              <button className='cartButton'><AiFillPlusCircle /></button>
-            </div>
-            <button className='cartDelete'><AiFillDelete /></button>
-          </div>
-        </div>
-      ))}
+      {cart.length === 0 && <h2>Még nem vásároltál semmit sem ! <Link to="/">Hopp </Link></h2>}
+      {cart.length !== 0 && (
       <div>
+        {cart.map((cartItem, index) => (
+          <div key={index} className='cartItem'>
+            <div className='cartImage'>
+              <img
+                src={cartItem.image}
+                alt={cartItem.name}
+              />
+            </div>
+            <div className='cartTitle'>
+              <p>{cartItem.title.substring(0, 20)}...</p>
+              <p>${cartItem.price}</p>
+              <div className='cartButtons'>
+                <button className='cartButton'><AiFillMinusCircle /></button>
+                <span className='cartSpan'>1</span>
+                <button className='cartButton'><AiFillPlusCircle /></button>
+              </div>
+              <button className='cartDelete'><AiFillDelete /></button>
+            </div>
+          </div>
+        ))}
+        <div>
+        </div>
         <p>Összesen: 232434</p>
       </div>
+     
+     
+       )}
+        
+      
+   
     </div>
   )
 }
