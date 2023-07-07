5adb49004c2fbdfae7cefa8ab9c9ef2ea55038d7
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-07-07 18:52:41 +0200

    addToCart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -7,15 +7,44 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   console.log(cart)
 
-  
+
+
+
   const addToCart = (product) => {
-    setCart([...cart, { ...product, quantity: 1 }]);
+    const existingProduct = cart.find(item => item.id === product.id);
+
+    if (existingProduct) {
+      const updatedCart = cart.map(item => {
+        if (item.id === product.id) {
+          return { ...item, quantity: item.quantity + 1 };
+        }
+       
+      });
+
+      setCart(updatedCart);
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }]);
+      console.log(product.quantity)
+    }
   };
+
+  const removeFromCart = (product) => {
+   
+  };
+
+  const plusQuantity =(product) =>{
+   
+  }
+
+  const minusQuantity =(product) =>{
+   
+  }
+
 
 
 
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, addToCart,removeFromCart,plusQuantity,}}>
       {children}
     </CartContext.Provider>
   );
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,42 +7,43 @@ import { AiFillDelete } from 'react-icons/ai'
 import { Link } from 'react-router-dom';
 
 export default function Cart() {
-  const { cart } = useContext(CartContext);
+  const { cart, removeFromCart,plusQuantity,minusQuantity} = useContext(CartContext);
   return (
     <div className='cartContainer'>
       {cart.length === 0 && <h2>Még nem vásároltál semmit sem ! <Link to="/">Hopp </Link></h2>}
       {cart.length !== 0 && (
-      <div>
-        {cart.map((cartItem, index) => (
-          <div key={index} className='cartItem'>
-            <div className='cartImage'>
-              <img
-                src={cartItem.image}
-                alt={cartItem.name}
-              />
-            </div>
-            <div className='cartTitle'>
-              <p>{cartItem.title.substring(0, 20)}...</p>
-              <p>${cartItem.price}</p>
-              <div className='cartButtons'>
-                <button className='cartButton'><AiFillMinusCircle /></button>
-                <span className='cartSpan'>1</span>
-                <button className='cartButton'><AiFillPlusCircle /></button>
-              </div>
-              <button className='cartDelete'><AiFillDelete /></button>
-            </div>
-          </div>
-        ))}
         <div>
+          {cart.map((cartItem, index) => (
+            <div key={index} className='cartItem'>
+              <div className='cartImage'>
+                <img
+                  src={cartItem.image}
+                  alt={cartItem.name}
+                />
+              </div>
+              <div className='cartTitle'>
+                <p>{cartItem.title.substring(0, 20)}...</p>
+                <p>${cartItem.price}</p>
+                <div className='cartButtons'>
+                  <button className='cartButton' onClick={() =>minusQuantity(cartItem)}><AiFillMinusCircle /></button>
+                  <span className='cartSpan'>{cartItem.quantity}</span>
+                  <button className='cartButton' onClick={() =>plusQuantity(cartItem) }><AiFillPlusCircle /></button>
+                </div>
+                <button className='cartDelete' onClick={() => removeFromCart(cartItem)}><AiFillDelete /></button>
+                
+              </div>
+            </div>
+          ))}
+          <div>
+          </div>
+          <p>Full Price: {cart.reduce((total, item) => total + (item.quantity * item.price), 0)}</p>
         </div>
-        <p>Összesen: 232434</p>
-      </div>
-     
-     
-       )}
-        
-      
-   
+
+
+      )}
+
+
+
     </div>
   )
 }
